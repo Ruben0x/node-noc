4b0c1076fdc8870af324033dabac79b8c3ad999f
ruben <r.alejandro.gn@gmail.com>
2024-07-30 17:42:58 -0400

    Final seccion 9

--- src/domain/use-cases/checks/check-service.ts
+++ src/domain/use-cases/checks/check-service.ts
@@ -1,3 +1,6 @@
+import { LogEntity, LogSeverityLevel } from '../../entities/log.entity';
+import { LogRepository } from '../../repository/log.repository';
+
 interface CheckServiceUseCase {
   execute(url: string): Promise<boolean>;
 }
@@ -7,6 +10,7 @@ type ErrorCallback = (error: string) => void;
 
 export class CheckService implements CheckServiceUseCase {
   constructor(
+    private readonly logRepository: LogRepository,
     private readonly successCallback: SuccessCallback,
     private readonly errorCallback: ErrorCallback
   ) {}
@@ -17,11 +21,14 @@ export class CheckService implements CheckServiceUseCase {
 
       if (!req.ok) throw new Error(`Error on check service ${url}`);
 
+      const log = new LogEntity(`Service ${url} working`, LogSeverityLevel.low);
+      this.logRepository.saveLog(log);
       this.successCallback();
       return true;
     } catch (error) {
-      console.log(`${error}`);
-
+      const errorMessage = `${error} at ${url}`;
+      const log = new LogEntity(errorMessage, LogSeverityLevel.high);
+      this.logRepository.saveLog(log);
       this.errorCallback(`${error}`);
       return false;
     }
